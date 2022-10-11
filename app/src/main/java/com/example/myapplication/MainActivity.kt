6b82017f4279274c13b705ff17b64f0e9f486ebe
Qukunov Yuriy <kukunovyura29@gmail.com>
2022-10-11 17:35:13 +0400

    Added logics for fragment_three

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -59,6 +59,8 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+
     }
 
     fun fragment(fragment: Fragment, id: Int) {
--- app/src/main/java/com/example/myapplication/fragment/Fragment_for_three_bal.kt
+++ app/src/main/java/com/example/myapplication/fragment/Fragment_for_three_bal.kt
@@ -1,30 +1,122 @@
 package com.example.myapplication.fragment
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.BoringLayout.make
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentForThreeBalBinding
+import android.widget.Toast.makeText as makeText1
 
 
 class Fragment_for_three_bal : Fragment() {
 
 
-
+    lateinit var binding: FragmentForThreeBalBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentForThreeBalBinding.inflate(inflater)
+        binding = FragmentForThreeBalBinding.inflate(inflater)
+
+
         return binding.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = Fragment_for_three_bal()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+        binding.buttonResult.setOnClickListener(View.OnClickListener {
+            if (!isEmptyEditText()) {
+                binding.TVForResult.text = result()
+            }
+        })
+
+
     }
+
+
+
+
+    private fun isEmptyEditText(): Boolean {
+
+
+        binding.apply {
+            if (editTextForBalOne.text.isNullOrEmpty()) editTextForBalOne.error =
+                "Заполните поле значением от 1 до 100"
+            if (editTextForKreditOne.text.isNullOrEmpty()) editTextForKreditOne.error =
+                "Заполните поле"
+
+            if (editTextForBalTwo.text.isNullOrEmpty()) editTextForBalTwo.error =
+                "Заполните поле значением от 1 до 100"
+            if (editTextForKreditTwo.text.isNullOrEmpty()) editTextForKreditTwo.error =
+                "Заполните поле"
+
+            if (editTextForBalThree.text.isNullOrEmpty()) editTextForBalThree.error =
+                "Заполните поле значением от 1 до 100"
+            if (editTextForKreditThree.text.isNullOrEmpty()) editTextForKreditThree.error =
+                "Заполните поле"
+
+            return editTextForBalOne.text.isNullOrEmpty()
+                    || editTextForKreditOne.text.isNullOrEmpty()
+                    || editTextForBalTwo.text.isNullOrEmpty()
+                    || editTextForKreditTwo.text.isNullOrEmpty()
+                    || editTextForBalThree.text.isNullOrEmpty()
+                    || editTextForKreditThree.text.isNullOrEmpty()
+        }
+    }
+
+
+    private fun result (): String {
+        val balOne: Double
+        val kreditOne: Double
+        val balTwo: Double
+        val kreditTwo: Double
+        val balThree: Double
+        val kreditThree: Double
+
+        var result: Double = 0.0
+
+            balOne = binding.editTextForBalOne.text.toString().toDouble()
+            kreditOne = binding.editTextForKreditOne.text.toString().toDouble()
+
+            balTwo = binding.editTextForBalTwo.text.toString().toDouble()
+            kreditTwo = binding.editTextForKreditTwo.text.toString().toDouble()
+
+            balThree = binding.editTextForBalThree.text.toString().toDouble()
+            kreditThree = binding.editTextForKreditThree.text.toString().toDouble()
+
+      //  if(balOne<=100 || balTwo <=100 || balThree<=100) {
+            result = (((balOne*kreditOne)+(balTwo*kreditTwo)+(balThree*kreditThree))/(kreditOne+kreditTwo+kreditThree))
+      //  }
+      //  else{
+       // }
+
+        return result.toString()
+
+    }
+
+
+        companion object {
+            @JvmStatic
+            fun newInstance() = Fragment_for_three_bal()
+        }
+
+
+
 }
+
